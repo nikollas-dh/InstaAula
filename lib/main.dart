3b2bafda5b5ad618b274c6a98d436051fd47a616
= <nikollasdheyvis17@gmail.com>
2026-09-23 10:20:04 -0300

    adicionando assets ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Instagram Nikollas',
+      title: 'InstaAula',
       home: const LoginPage(),
     );
   }
